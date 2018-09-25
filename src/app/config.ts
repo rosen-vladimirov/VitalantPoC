74ee8f4a4f5af07e61280876d54ad812d3ad33e2
Ignacio Fuentes <ignaciofuentes@outlook.com>
2018-09-25 16:18:15 -0400

    generic logo

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,5 @@
 export class Config {
   static collectionName = "products";
+  static logo: string =
+    "https://devcenter.kinvey.com/images/Progress_Kinvey_Primary.png";
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { DrawerHelper } from "../utils/drawer-helper";
+import { Config } from "../config";
 
 @Component({
   selector: "Home",
   templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+  logo: string;
   constructor() {
     // Use the component constructor to inject providers.
   }
 
   ngOnInit(): void {
+    this.logo = Config.logo;
     // Init your component properties here.
   }
 
