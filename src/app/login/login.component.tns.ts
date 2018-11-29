31d854fb8a478253574200ba2695ef470eddc599
Thomas Conner <thomas.conner@me.com>
2018-11-29 08:43:28 -0500

    Use sde:// as the redirectUri

--- src/app/login/login.component.tns.ts
+++ src/app/login/login.component.tns.ts
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
   async loginWithMIC() {
     try {
       this.processing = true;
-      await this.dataService.loginWithMIC();
+      await this.dataService.loginWithMIC('sde://');
       this.router.navigate([""]);
     } catch {
       alert("Invalid credentials");
